ec915840ece46960f2cf73e4caf0bca429708d60
beyzakilickol <43250495+beyzakilickol@users.noreply.github.com>
2019-02-05 23:37:40 -0600

    pusher is set and functioning

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react'
 import {Platform,Text, View,Button, TextInput,ScrollView} from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat'
+import { ChatManager, TokenProvider } from '@pusher/chatkit-client';
 import styles from '../styles'
+
+const CHATKIT_TOKEN_PROVIDER_ENDPOINT = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/e1fc9d21-95df-426a-87e1-462a9266a022/token';
+const CHATKIT_INSTANCE_LOCATOR = 'v1:us1:e1fc9d21-95df-426a-87e1-462a9266a022';
+const CHATKIT_ROOM_ID = '19379252';
+const CHATKIT_USER_NAME = 'CoolDeveloper';
+const CHATKIT_USER_NAME2 = 'Beyza';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -15,24 +22,84 @@ export default class App extends Component{
     constructor(props){
         super(props)
         this.state={
-            messsage: ['heyo']
+            messages: []
         }
     }
-    onSend(messages = []) {
-        this.setState(previousState => ({
-          messages: GiftedChat.append(previousState.messages, messages),
-        }))
+    componentDidMount() {
+        const tokenProvider = new TokenProvider({
+          url: CHATKIT_TOKEN_PROVIDER_ENDPOINT,
+        });
+    
+        const chatManager = new ChatManager({
+          instanceLocator: CHATKIT_INSTANCE_LOCATOR,
+          userId: CHATKIT_USER_NAME2,
+          tokenProvider: tokenProvider,
+        });
+    
+        chatManager
+          .connect()
+          .then(currentUser => {
+            this.currentUser = currentUser;
+            this.currentUser.subscribeToRoom({
+              roomId: CHATKIT_ROOM_ID,
+              hooks: {
+                onMessage: this.onReceive,
+              },
+            });
+          })
+          .catch(err => {
+            console.log(err);
+          });
       }
+      onReceive = data => {
+        const { id, senderId, text, createdAt } = data;
+        const incomingMessage = {
+          _id: id,
+          text: text,
+          createdAt: new Date(createdAt),
+          user: {
+            _id: senderId,
+            name: senderId,
+            avatar:
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmXGGuS_PrRhQt73sGzdZvnkQrPXvtA-9cjcPxJLhLo8rW-sVA',
+          },
+        };
+    
+        this.setState(previousState => ({
+          messages: GiftedChat.append(previousState.messages, incomingMessage),
+        }));
+      };
+    // onSend(messages = []) {
+    //     this.setState(previousState => ({
+    //       messages: GiftedChat.append(previousState.messages, messages),
+    //     }))
+    //   }
+    onSend = (messages = []) => {
+        messages.forEach(message => {
+          this.currentUser
+            .sendMessage({
+              text: message.text,
+              roomId: CHATKIT_ROOM_ID,
+            })
+            .then(() => {})
+            .catch(err => {
+              console.log(err);
+            });
+        });
+      };
     render() {
       return (
         
         <View style={styles.mainContainer}>
         
-         <GiftedChat
-        messages={this.state.message}
+         <GiftedChat style={styles.chatDiv}
+         renderAvatarOnTop = {true}
+         bottomOffset= {300}
+         placeholder={'Please type your message here..'}
+        messages={this.state.messages}
         onSend={messages => this.onSend(messages)}
         user={{
-          _id: 1,
+          _id: CHATKIT_USER_NAME2,
         }}
       />
         </View>
--- styles.js
+++ styles.js
@@ -1,33 +1,14 @@
 import {StyleSheet} from 'react-native'
 
 export default StyleSheet.create({
-    textColor:{
-        color:'black',
-        marginTop: 70,
-        marginLeft: 20,
-      },
-      topDiv: {
-         flex: 0.5,
-         backgroundColor: 'yellow',
-         margin:'5%',
-         borderRadius: 15,
-         shadowOffset:{  width: 10,  height: 10,  },
-         shadowColor: 'cyan',
-         shadowOpacity: 1.0,
-         height: 500
-      },
+    
       mainContainer:{
         flex:1,
         height: '100%',
        
         backgroundColor:'white'
       },
-      input: {
-        borderStyle: 'solid',
-        borderColor: 'black',
-        borderWidth: 1,
-        width: 100,
-        height: 20,
-        textAlign: 'center'
-      }
+     chatDiv :{
+         marginBottom: 140
+     }
 })
